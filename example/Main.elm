98491c2d922449cdc52d6013789d6a8faf9d5cde
Vivien Alger <vivien@nomalab.com>
2017-04-06 11:30:18 +0200

    Passing example test

--- example/Main.elm
+++ example/Main.elm
@@ -9,37 +9,21 @@ port emit : PortEvent -> Cmd msg
 
 main : WebdriverRunner
 main =
-    run emit basicOptions <| group "Find in Google" [ searchElm, searchHackerNews ]
-
+    run emit basicOptions <| group "Find in Google" [ searchElm ]
 
 firstLink_ : String
 firstLink_ =
     "#rso > div:nth-child(1) > div:nth-child(1) > div > h3 > a"
 
-
 searchElm : Run
 searchElm =
     describe "Finding Elm Lang in Google"
         [ visit "https://google.com"
-        , title <| Expect.equal "this will fail!"
-        , elementCount "input[name='q']" <| Expect.atLeast 1
-        , setValue "input[name='q']" "Elm lang"
-        , elementText firstLink_ <| Expect.equal "Elm is the best"
-        , click firstLink_
-        , pause 1000 |> withScreenshot True
-        , title <| Expect.equal "home"
-        ]
-
-
-searchHackerNews : Run
-searchHackerNews =
-    describe "Finding hacker news in Google"
-        [ visit "https://google.com"
         , title <| Expect.equal "Google"
         , elementCount "input[name='q']" <| Expect.atLeast 1
-        , setValue "input[name='q']" "Hacker News"
-        , elementText firstLink_ <| Expect.equal "Hacker News"
+        , setValue "input[name='q']" "Elm lang"
+        , elementText firstLink_ <| Expect.equal "Elm"
         , click firstLink_
-        , pause 1000
+        , pause 1000 |> withScreenshot True
         , title <| Expect.equal "home"
         ]
